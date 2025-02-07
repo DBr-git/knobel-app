7b1925d63ce864493e0ad2a3dcfdcf9f838432d7
Dennis <d-l-b@gmx.de>
2025-02-07 21:12:28 +0100

    added login links

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,14 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { UserProvider } from "@auth0/nextjs-auth0/client";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <UserProvider>
+      <Head>
+        <title>Kobel Kasse</title>
+      </Head>
       <Component {...pageProps} />
     </UserProvider>
   );
